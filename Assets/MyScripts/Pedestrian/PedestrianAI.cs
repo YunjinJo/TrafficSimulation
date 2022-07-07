ba16dcc66ee2c8a1c8eb3e0116f50b1df00710b1
black33jo <black33jo@gmail.com>
2022-07-07 10:09:02 +0900

    Add Pedestrian AI & NaveMesh for test

--- Assets/MyScripts/Pedestrian/PedestrianAI.cs
+++ Assets/MyScripts/Pedestrian/PedestrianAI.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PedestrianAI : MonoBehaviour
 {
+    private NavMeshAgent _agent;
+
+    public Transform[] waypoints;
+
+    private int waypointIndex;
+
+    private Vector3 target;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _agent = GetComponent<NavMeshAgent>();
+        UpdateDestination();
+    }
+
+    private void UpdateDestination()
+    {
+        target = waypoints[waypointIndex].position;
+        _agent.SetDestination(target);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector3.Distance(transform.position, target) < 1)
+        {
+            UpdateDestination();
+            IterateWaypointIndex();
+            
+        }
+    }
+
+    void IterateWaypointIndex()
+    {
+        waypointIndex++;
+        if (waypointIndex == waypoints.Length)
+        {
+            Destroy(this.GameObject());
+        }
     }
 }
